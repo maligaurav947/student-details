b9be3c39c15e0a6156bc1eac117fb8acadaa670c
Mali Gaurav <maligaurav947@gmail.com>
2023-02-26 20:55:59 +0530

    V0.01

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -4,22 +4,24 @@ import Contact from "../Contact/Contact";
 import Header from "../Header/Header";
 import Feature from "./Feature";
 import Testimonials from "./Testimonials";
-
+var eye = require("../asst/img/MonstersEye.png");
+var BrooklynRelaxing = require("../asst/img/People of BrooklynRelaxing.png");
 function Home() {
   return (
     <>
       <Marquees />
       <Header />
-
       <div
-        className="bg-red-300 text-white w-screen text-center grid place-content-center bg-no-repeat"
+        className="w-screen text-center grid place-content-center bg-no-repeat"
         style={{
           backgroundSize: "Cover",
           height: "80vh",
-          backgroundImage: `url(https://burst.shopifycdn.com/photos/moody-green-vine-wall-texture.jpg?width=925&format=pjpg&exif=1&iptc=1)`,
+
           backgroundAttachment: "fixed",
         }}
       >
+        <img src={eye} className="absolute right-24 w-28 top-48" />
+        <img src={BrooklynRelaxing} className="left-24 w-72 top-96 absolute" />
         <h2 className="text-6xl font-medium mb-3">
           Welcome to the Student Details System
         </h2>
