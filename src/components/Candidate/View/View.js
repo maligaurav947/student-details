d6545cd0329272fdf06f5632727121423015a266
Mali Gaurav <maligaurav947@gmail.com>
2023-03-03 09:28:49 +0530

    View ui v0.2

--- src/components/Candidate/View/View.js
+++ src/components/Candidate/View/View.js
@@ -23,10 +23,10 @@ function View() {
   return (
     <>
       <Header />
-      <div className="flex flex-col items-center justify-center gap-8 h-full">
+      <div className="flex mt-2 flex-col items-center justify-center gap-5 h-full">
         <div className="grid md:lg:xl:2xl:flex gap-2 items-center justify-center">
           <div className="">
-            <div className="w-full grid place-content-center text-center mr-2">
+            <div className="w-full grid place-content-center text-center mr-2 ">
               <div className="w-full grid place-content-center">
                 {user.url ? (
                   <img src={user.url} className="w-40 h-40 rounded-md" />
@@ -56,32 +56,38 @@ function View() {
               <div></div>
             </div>
           </div>
-          <span className="lg:md:xl:2xl:border-l-8 border-black">
-            <div className="ml-2 p-1 grid gap-5">
-              <div className="text-lg">Address:-{user.address}</div>
-              <div className="text-lg">Email ID:-{user.email}</div>
-              <div className="text-lg">Phone Number:-{user.contact}</div>
-              <div className="text-lg">Department:-{user.department}</div>
-              <div className="text-lg">
+          <span className="lg:md:xl:2xl:border-l-8 border-black ">
+            <div className="ml-2 p-1 grid gap-1 ">
+              <div className="text-lg bg-blue-200 p-1">
+                Address:-{user.address}
+              </div>
+              <div className="text-lg  p-1">Email ID:-{user.email}</div>
+              <div className="text-lg bg-blue-200 p-1">
+                Phone Number:-{user.contact}
+              </div>
+              <div className="text-lg p-1">Department:-{user.department}</div>
+              <div className="text-lg bg-blue-200 p-1">
                 Collage:-Shri Jaykumar Rawal Institute of Technology, Dondaicha
               </div>
             </div>
           </span>
         </div>
-        <div className="flex xl:md:lg:2xl:w-2/3 items-center justify-center w-full p-2 text-base text-justify xl:md:lg:2xl:text-center">
-          Shri Jaykumar Rawal Institute of Technology was set up as a part of
-          the self-powered plans of Dondaicha Dadasaheb knowledge city in 2011
-          with the objective to erect a truly world class institute in the rural
-          part where students from the vicinity would be benefited from the
-          services that matched global standards. SJRIT was conceived by
-          visionary leader, Hon. Shri Jaykumar Bhau Rawal , former cabinet
-          minister for environment for Maharashtra State. The institute has
-          grown at a remarkable pace and has gone on to become an institute of
-          repute within the country.
+        <div className="flex bg-blue-200 items-center justify-center w-full p-4 text-base text-justify ">
+          <div className="xl:md:lg:2xl:w-2/3">
+            Shri Jaykumar Rawal Institute of Technology was set up as a part of
+            the self-powered plans of Dondaicha Dadasaheb knowledge city in 2011
+            with the objective to erect a truly world class institute in the
+            rural part where students from the vicinity would be benefited from
+            the services that matched global standards. SJRIT was conceived by
+            visionary leader, Hon. Shri Jaykumar Bhau Rawal , former cabinet
+            minister for environment for Maharashtra State. The institute has
+            grown at a remarkable pace and has gone on to become an institute of
+            repute within the country.
+          </div>
         </div>
-        <div className="grid grid-cols-1 md:lg:xl:grid-cols-2 gap-10 place-content-center">
-          <div className="w-full h-12 flex items-center justify-between gap-5">
-            <h2 className="text-lg">Semester V</h2>
+        <div className="grid grid-cols-1 md:lg:xl:grid-cols-2 gap-5 place-content-center">
+          <div className="h-12 w-full flex items-center justify-between rounded-md pt-2 pb-2 pl-4 pr-4 gap-5 bg-blue-200">
+            <h2>Semester V</h2>
             <div>
               <h4>Attandance</h4>
               <h2>{user.semfive}/100</h2>
@@ -94,7 +100,7 @@ function View() {
               Download The Result
             </a>
           </div>
-          <div className="w-full h-12 flex items-center justify-between gap-5">
+          <div className="h-12 w-full flex items-center justify-between rounded-md pt-2 pb-2 pl-4 pr-4 gap-5 bg-blue-200">
             <h2>Semester VI</h2>
             <div>
               <h4>Attandance</h4>
@@ -108,7 +114,7 @@ function View() {
               Download The Result
             </a>
           </div>
-          <div className="w-full h-12 flex items-center justify-between gap-5">
+          <div className="h-12 w-full flex items-center justify-between rounded-md pt-2 pb-2 pl-4 pr-4 gap-5 bg-blue-200">
             <h2>Semester VII</h2>
             <div>
               <h4>Attandance</h4>
@@ -122,7 +128,7 @@ function View() {
               Download The Result
             </a>
           </div>
-          <div className="w-full h-12 flex items-center justify-between gap-5">
+          <div className="h-12 w-full flex items-center justify-between rounded-md pt-2 pb-2 pl-4 pr-4 gap-5 bg-blue-200">
             <h2>Semester VIII</h2>
             <div>
               <h4>Attandance</h4>
